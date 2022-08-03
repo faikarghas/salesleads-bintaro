9affce4b321c10acecbdadb87781d74a323f6b27
faikarghas <ghassanfaikar@yahoo.com>
2022-08-03 20:43:49 +0700

    add wa report

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -5,7 +5,8 @@ import excuteQuery from '../../utils/db'
 export default async function handler(req, res) {
 
   let data = await excuteQuery({
-    query: 'SELECT * FROM leads WHERE lead IS NULL AND email IS NULL ORDER BY created_at DESC'
+    query: `SELECT id , phone, source, promo, cluster, type, CONVERT_TZ(created_at,'+00:00','+07:00') as created_at
+     FROM leads WHERE lead IS NULL AND email IS NULL ORDER BY created_at DESC`
   })
 
   res.status(200).json({data:data})
