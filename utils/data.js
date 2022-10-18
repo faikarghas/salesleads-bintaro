ae4ac73eac799459b95b81d46e389803d2daab18
faikarghas <ghassanfaikar@yahoo.com>
2022-10-18 19:15:23 +0700

    fix update pipeline for spa sign and add wb for prod

--- utils/data.js
+++ utils/data.js
@@ -1,6 +1,6 @@
 export const list_filter = {
     filter_1: ['Favorit'],
-    filter_2: ['New','Pending Response','Contacted','Gallery Visit','Negotiation','Booked','Spa Signed','Lost'],
+    filter_2: ['New','Pending Response','Contacted','Gallery Visit','Negotiation','Booked','SPA Sign','Lost'],
     filter_3: ['Hot','Warm','Cold','Close'],
     filter_4: ['Tidak Ada Tugas Mendatang','Tugas Yang Terlambat'],
 }
